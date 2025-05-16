0adab8d576e812e7551806670c8d898737c3344a
joaopedro-style <joaopedro18231@hotmail.com>
2025-05-16 09:29:10 -0300

    Update Utils.php

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -7,7 +7,6 @@ final class Utils {
 private function __construct() {
     
 }
-
     public static function codificarSenha(string $senha):string {
         return password_hash($senha, PASSWORD_DEFAULT);
     }
