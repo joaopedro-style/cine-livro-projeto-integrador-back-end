038324c8bf7d8de8db3c10b08763e250766e676f
joaopedro-style <joaopedro18231@hotmail.com>
2025-05-16 21:43:34 -0300

    Create Filme.php

---
+++ src/Models/Filme.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CineLivro\Models;
+
+final class Filme
+{
+    private ?int $id;
+    private string $titulo;
+    private string $diretor;
+    private string $data_lancamento;
+    private int $duracao;
+}
