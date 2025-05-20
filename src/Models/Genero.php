a95a5171d60942796333e036262e547e171e8b05
joaopedro-style <joaopedro18231@hotmail.com>
2025-05-19 21:56:27 -0300

    código e diagrama finalizados

---
+++ src/Models/Genero.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CineLivro\Models;
+
+final class Genero
+{
+    private ?int $id;
+    private string $nome;
+
+    public function __construct(string $nome, ?int $id = null)
+    {
+        $this->setId($id);
+        $this->setNome($nome);
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    private function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    private function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+}
